eb6961bdbc6600728c6c41d87a8f81bba1a4734e
Hansika Shishodia <54855577+Hansika94@users.noreply.github.com>
2019-09-08 17:24:28 +0100

    Update Footer.js

--- components/template/Footer.js
+++ components/template/Footer.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Container, Divider, List, Segment } from 'semantic-ui-react';
-import { Link } from '../../routes';
 
 class Footer extends Component {
     render() {
