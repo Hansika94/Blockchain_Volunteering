9a11227c884708855c14a507f67b1bfd0e74a3d6
Hansika Shishodia <54855577+Hansika94@users.noreply.github.com>
2019-09-08 17:54:45 +0100

    Update volunteerOptions.js

--- pages/volunteerOptions.js
+++ pages/volunteerOptions.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { Container, Header } from "semantic-ui-react";
 import Layout from "../components/template/Layout";
 import VolunteerOptionsTable from "../components/volunteer/VolunteerOptionsTable";
-import { Link, Router } from "../routes";
 
 class VolunteerOptionsPage extends Component {
     render() {
