02f5164311aa5da5b33cfe356b084c564eae240e
Hansika Shishodia <54855577+Hansika94@users.noreply.github.com>
2019-09-08 16:55:19 +0100

    Create approveOrg.js

---
+++ pages/approve/approveOrg.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Header } from 'semantic-ui-react';
+import { Link, Router } from '../../routes';
+import Layout from '../../components/template/Layout';
+import AdminApproveOrg from '../../components/admin/AdminApproveOrg';
+
+
+class ApproveOrgPage extends Component {
+
+    render () {
+        return(
+            <Layout>
+                <Header as='h1'>Approve Organisation page</Header>
+                <br/>
+                <p>Hello admin, here you can approve the organisation request to become official.</p>
+                <AdminApproveOrg/>
+            </Layout>
+        );
+    }
+}
+
+export default ApproveOrgPage;
