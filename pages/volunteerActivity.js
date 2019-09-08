3792abad412051b1656fc4fbd47c012060e28a66
Hansika Shishodia <54855577+Hansika94@users.noreply.github.com>
2019-09-08 17:54:21 +0100

    Update volunteerActivity.js

--- pages/volunteerActivity.js
+++ pages/volunteerActivity.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { Container, Header } from "semantic-ui-react";
 import Layout from "../components/template/Layout";
 import ActivityForm from "../components/organisation/ActivityForm";
-import { Link, Router } from "../routes";
 
 class VolunteerActivityPage extends Component {
     render() {
